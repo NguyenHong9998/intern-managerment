9609f96e87370621e0ff62795b9801a3217e2c00
NguyenHong9998 <nguyenhong19999@gmail.com>
2021-05-08 21:02:54 +0700

    fix bug

--- src/main/java/cnpm/doan/service/UserServiceImpl.java
+++ src/main/java/cnpm/doan/service/UserServiceImpl.java
@@ -107,9 +107,9 @@ public class UserServiceImpl implements UserService {
 
         User user = userRepository.findById(idUser).orElse(null);
         if (user != null) {
-            if (user.getRoles().getRoleName().equals("ROLE_MANAGER")) {
-                throw new CustormException(Message.CANNOT_DELETE_MANAGER);
-            }
+//            if (user.getRoles().getRoleName().equals("ROLE_MANAGER") ) {
+//                throw new CustormException(Message.CANNOT_DELETE_MANAGER);
+//            }
             List<MemberTask> memberTasks = memberTaskRepository.findAll().stream().filter(t -> t.getUser().getId() == user.getId()).collect(Collectors.toList());
             for (MemberTask memberTask : memberTasks) {
                 if (!memberTask.getTask().isDone()) {
