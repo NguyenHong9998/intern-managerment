09ca2ffe48c14d28ef328b1e1d9301d36dec77d0
NguyenHong9998 <nguyenhong19999@gmail.com>
2021-05-09 12:20:11 +0700

    feedback domain

--- src/main/java/cnpm/doan/domain/FeedbackResponseDomain.java
+++ src/main/java/cnpm/doan/domain/FeedbackResponseDomain.java
@@ -1,8 +1,40 @@
 package cnpm.doan.domain;
 
+import cnpm.doan.entity.Feedback;
+import cnpm.doan.util.DatetimeUtils;
+
 public class FeedbackResponseDomain {
     private int taskId;
     private String feedbackContent;
+    private String date;
+    private int feedbackId;
+
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public FeedbackResponseDomain() {
+
+    }
+
+    public FeedbackResponseDomain(Feedback feedback) {
+        this.taskId = feedback.getTask().getId();
+        this.date = DatetimeUtils.convertDateToString(feedback.getTime());
+        this.feedbackContent = feedback.getMessage();
+        this.feedbackId = feedback.getId();
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
 
     public int getTaskId() {
         return taskId;
