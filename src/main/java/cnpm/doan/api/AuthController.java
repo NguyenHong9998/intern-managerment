57efd5b36a2f765db48891f2db9bb37a9442e36d
NguyenHong9998 <nguyenhong19999@gmail.com>
2021-05-09 22:56:33 +0700

    change pass

--- src/main/java/cnpm/doan/api/AuthController.java
+++ src/main/java/cnpm/doan/api/AuthController.java
@@ -48,7 +48,7 @@ public class AuthController {
     public ResponseEntity<?> changePass(@RequestBody ChangePasswordRequest changePasswordRequest) {
         System.out.println("old: " + changePasswordRequest.getOldPassword() + "new: " + changePasswordRequest.getNewPassword());
         User user = userService.findById(jwtUtil.getCurrentUser().getUserId());
-        if (!changePasswordRequest.getOldPassword().equals(new BCryptPasswordEncoder().encode(user.getPassword()))) {
+        if (!new BCryptPasswordEncoder().encode(changePasswordRequest.getOldPassword()).equals(new BCryptPasswordEncoder().encode(user.getPassword()))) {
             return ResponseEntity.ok(new ResponeDomain(Message.INVALID_OLD_PASS.getDetail(), false));
         }
         user.setPassword(new BCryptPasswordEncoder().encode(changePasswordRequest.getNewPassword()));
