d66e005bbd76352958c5397df32d5f2c903640ee
NguyenHong9998 <nguyenhong19999@gmail.com>
2021-05-15 15:02:14 +0700

    fix

--- src/main/java/cnpm/doan/repository/FeedbackRepository.java
+++ src/main/java/cnpm/doan/repository/FeedbackRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-    @Query("select new Feedback (f.id, f.message,  f.time, f.user,  f.task )from Feedback f where f.task.id = ?1 order by f.time desc ")
     List<Feedback> findFeedbackByTaskId(int taskId);
 
     void deleteByTaskId(int taskId);
